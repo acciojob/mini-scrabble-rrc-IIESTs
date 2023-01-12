4bd931d79d8a1e1ee7f32887042f7da96e75ce7b
Reetu Raj Chauhan <111192512+rrc-IIESTs@users.noreply.github.com>
2023-01-12 21:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const x= document.getElementById('evaluatedText')
+x.addEventListener('keyup',display);
+function display(){
+	const head = document.getElementById('letterCount');
+	head.innerHTML=x.value.length;
+}
+
+
+
+
